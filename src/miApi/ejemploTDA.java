d349b33cf7e211f70222473c5b7ca4b38ea9f71d
mariancoto <68799441+mariancoto@users.noreply.github.com>
2020-09-18 11:07:41 -0300

    Hola

--- src/miApi/ejemploTDA.java
+++ src/miApi/ejemploTDA.java
@@ -1,6 +1,7 @@
 package miApi;
 
 // En este directorio vamos a guardar los archivos de TDA (en archivos separados)
+// ayyyy
 
 public class ejemploTDA {
     // Imaginense que esto es un TDA jajaj
