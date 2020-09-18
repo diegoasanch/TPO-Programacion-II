ea291e7f04772eadedc79e4da957802d6b88b17f
mariancoto <68799441+mariancoto@users.noreply.github.com>
2020-09-18 11:19:38 -0300

    jj
    jj

--- src/misAplicaciones/ejemploApp.java
+++ src/misAplicaciones/ejemploApp.java
@@ -9,5 +9,4 @@ public class ejemploApp {
         System.out.println("Si todo salio bien podrian correr el programa y ver este mesaje en la consola.");
 
     }
-    System.out.println("Hello world!\n");
 }
