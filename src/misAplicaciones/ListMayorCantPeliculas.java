0ebbca33a2ff79ea31674f4e375fc75ac1108b64
Diego Sanchez <diegoasp18@gmail.com>
2020-11-09 13:39:05 -0300

    Terminado ej 2, organizado programa principal

--- src/misAplicaciones/ListMayorCantPeliculas.java
+++ src/misAplicaciones/ListMayorCantPeliculas.java
@@ -2,9 +2,10 @@ package misAplicaciones;
 import miApi.ColaPrioridadTDA;
 import miApi.DiccionarioSimpleTDA;
 import misAlgoritmos.MetodosColaPrioridad;
+import misImplementaciones.ColaPrioridad;
 
 public class ListMayorCantPeliculas {
-	public void Llevarconteo(DiccionarioSimpleTDA registro, int idPelicula){
+	public void llevarconteo(DiccionarioSimpleTDA registro, int idPelicula){
 		int entradas = 1;
         // Si la pelicula ya existe sacamos su valor anterior y le sumamos 1, eliminamos esa clave
         if (registro.claves().pertenece(idPelicula)) {
@@ -14,23 +15,41 @@ public class ListMayorCantPeliculas {
         // Creamos nuevo elemento con clave= idPelicula y las entradas totales
         registro.agregar(idPelicula, entradas);
 	}
-	
-	 public void imprimeSolicitudes(ColaPrioridadTDA masSolicitados) {
-	        MetodosColaPrioridad metodos = new MetodosColaPrioridad();
-	        ColaPrioridadTDA copiaSolicitudes = metodos.copiaCola(masSolicitados);
 
-	        int persona, cantidad, masSolicitada;
-	        masSolicitada = cantidad = copiaSolicitudes.primeraPrioridad();
-	        
-	        System.out.println("\nPeliculas mas solicitadas:\n");
+	public void imprimeSolicitudes(ColaPrioridadTDA masSolicitados) {
+		ColaPrioridadTDA reporte = extraeTop(masSolicitados);
+		int persona, cantidad;
+		System.out.println("\nPersonas que solicitaron mas veces:\n");
 
-	        while (cantidad == masSolicitada) {
+		while (!reporte.colaVacia()) {
+			cantidad= reporte.primerValor();
+			persona = reporte.primeraPrioridad();
 
-	            persona = copiaSolicitudes.primerValor();
-	            copiaSolicitudes.desacolar();
-	            System.out.printf("Persona %d solicito %d veces%n",persona, cantidad);
+			reporte.desacolar();
+			System.out.printf("  -Persona %d solicito %d veces%n",persona, cantidad);
+		}
+	}
 
-	            cantidad = copiaSolicitudes.primeraPrioridad();
-	        }
-	    }
+	public ColaPrioridadTDA extraeTop(ColaPrioridadTDA solicitantes) {
+
+		MetodosColaPrioridad metodos = new MetodosColaPrioridad();
+		ColaPrioridadTDA copiaSolicitudes = metodos.copiaCola(solicitantes);
+
+		int cantidad, masSolicitada, persona;
+		masSolicitada = cantidad = copiaSolicitudes.primeraPrioridad();
+
+		ColaPrioridadTDA nueva = new ColaPrioridad();
+		nueva.inicializarCola();
+
+		while (cantidad == masSolicitada && !copiaSolicitudes.colaVacia()) {
+			persona = copiaSolicitudes.primerValor();
+			copiaSolicitudes.desacolar();
+
+			// Acolamos en cola prioridad, prioridad = ID persona
+			nueva.acolarPrioridad(cantidad, persona);
+
+			cantidad = copiaSolicitudes.primeraPrioridad();
+		}
+		return nueva;
+	}
 }
--- src/misAplicaciones/ProgramaPrincipal.java
+++ src/misAplicaciones/ProgramaPrincipal.java
@@ -1,4 +1,4 @@
-/** @author Diego Sanchez
+/** @author Diego Sanchez, Alexis Weigandt, Maxi Rosso, Mariano Coto
  * Programa principal
  */
 package misAplicaciones;
@@ -15,30 +15,26 @@ import misImplementaciones.DiccionarioSimple;
 import misImplementaciones.PilaInt;
 import misImplementaciones.DiccionarioMultiple;
 
-
 import misAlgoritmos.MetodosTabla;
 import misAlgoritmos.MetodosColaInt;
 
-
-
 public class ProgramaPrincipal {
     public static void main(String[] args) {
-        
+
         // Inicializamos los metodos que llevaran a cabo las funciones
         MetodosTabla metodosTablas = new MetodosTabla();
         MetodosColaInt metodosColasInt = new MetodosColaInt();
         ListMayorCantPeliculas solicitudesPeli = new ListMayorCantPeliculas();
         CantSolicitudes solicitudes = new CantSolicitudes();
         listadoMov listaUltimos = new listadoMov();
-        
-        
+
         // Inicializamos las Tablas y las Cargamos
         TablaTDA peliculas = metodosTablas.cargaTabla("archivos_de_prueba/ListadoPeliculas.txt");
         TablaTDA proveedores = metodosTablas.cargaTabla("archivos_de_prueba/StreamCia.txt");
-    
+
         // Creamos y cargamos la cola con los movimientos
         ColaIntTDA movimientos = metodosColasInt.cargarMovimientos("archivos_de_prueba/Movimientos.txt", peliculas, proveedores);
-        
+
         // Creamos el diccionario para el registro de las solicitudes
         DiccionarioSimpleTDA registroSolicitudes = new DiccionarioSimple();
         registroSolicitudes.inicializarDiccionarioSimple();
@@ -48,14 +44,11 @@ public class ProgramaPrincipal {
         // Creamos el diccionario multiple para el registro a quien se les solicito la pelicula
         DiccionarioMultipleTDA registroCompanias = new DiccionarioMultiple();
         registroCompanias.inicializarDiccionarioMultiple();
-        
+
         // Creamos la pila para los 10 ultimos movimientos
         PilaIntTDA pilaUltimos = new PilaInt();
         pilaUltimos.inicializarPila();
-        
-        
-        
-        
+
         int mov;
         int idPelicula, idProveedor, idPersona;
 
@@ -65,42 +58,38 @@ public class ProgramaPrincipal {
 
             mov = movimientos.primero();
             movimientos.desacolar();
-            
-            idPersona = mov / 1000000;
-            idProveedor = (mov / 10000) % 1000; 
-            idPelicula = mov % 10000;
-            
-            pilaUltimos.apilar(mov);
 
+            idPersona = mov / 1000000;
+            idProveedor = (mov / 10000) % 1000;
+            idPelicula = mov % 10000;
+
+            pilaUltimos.apilar(mov);
             solicitudes.conteoSolicitudes(registroSolicitudes, idPelicula);
             solicitudes.registraCompania(registroCompanias, idPelicula, idProveedor);
-            solicitudesPeli.Llevarconteo(diccSolicitudes, idPersona);
+            solicitudesPeli.llevarconteo(diccSolicitudes, idPersona);
         }
-        
+
         /** Aca al finalizar el ciclo se haran los llamados a los programas para que estos emitan
         su reporte */
-        
-        
+
         ColaPrioridadTDA masSolicitados = solicitudes.generaColaSolicitudes(registroSolicitudes);
-
         ColaPrioridadTDA personasMasSolicitudes = solicitudes.generaColaSolicitudes(diccSolicitudes);
-        
-        System.out.println('+' + "-".repeat(60) + '+');
-        // Item 2
-        solicitudesPeli.imprimeSolicitudes(personasMasSolicitudes);
-
         ColaStrTDA lista10Ultimos = listaUltimos.crearColaDiezUltimos(pilaUltimos, peliculas, proveedores);
-        
+
         // Item 1
+        System.out.println("+ item 1 " + "-".repeat(60) + '+');
         listaUltimos.mostrarColaStr(lista10Ultimos);
 
-        
-        System.out.println('+' + "-".repeat(60) + '+');
+        // Item 2
+        System.out.println("+ item 2 " + "-".repeat(60) + '+');
+        solicitudesPeli.imprimeSolicitudes(personasMasSolicitudes);
+
         // Item 3
+        System.out.println("+ item 3 " + "-".repeat(60) + '+');
         solicitudes.imprimeCompanias(registroCompanias, masSolicitados, peliculas);
-        
-        System.out.println('+' + "-".repeat(60) + '+');
+
         // Item 4
+        System.out.println("+ item 4 " + "-".repeat(60) + '+');
         solicitudes.topSolicitudes(masSolicitados, peliculas, 10);
     }
 }
